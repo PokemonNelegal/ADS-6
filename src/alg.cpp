a215970b9b634991e8c3c0a5a5488587734e0337
Plztest <66181183+Plztest@users.noreply.github.com>
2020-05-30 23:35:23 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -14,19 +14,28 @@ BST<string> makeTree(char* filename)
     string str="", tmp="";
     while (file >> str)
     {
-        for (int j = 0; j < str.size(); j++)
-        {
-            if ((str[j]>= 'A' && str[j] <= 'Z') || (str[j]>='a' && str[j] <= 'z'))
-            {
-                str[j] = (char)tolower(str[j]);
-                tmp += str[j];
-            }
-            else if (tmp!="")
-            {
-                tree1.add(tmp);
-                tmp="";
-            }
-        }
+        for (int j = 0; j < s.size(); j++)
+			{
+				flag = 0;
+				if ((str[j] >= 'a' && str[j] <= 'z') || (str[j] >= 'A' && str[j] <= 'Z'))
+					tmp += (char)tolower(s[j]);
+				else 
+				{
+						if (tmp != "")
+						{
+							tree1.add(tmp);
+							tmp = "";
+							flag = 1;
+							continue;
+						}
+				}
+			}
+			if (flag == 0 && tmp!="")
+			{
+				tree1.add(tmp);
+				tmp = "";
+			}
+	}
     }
     return tree1;
 }
