fdbab094d8208ec6ec3ab78924bec0e4eeab1d90
Hleb1234 <62662050+Hleb1234@users.noreply.github.com>
2020-05-31 19:54:58 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -6,5 +6,36 @@
 
 BST<std::string> makeTree(char* filename)
 {
+  std::string word;
+	std::ifstream file(filename);
+	BST<std::string>* tree = new BST<std::string>;
+	char tempStr = ' ';
+	while (!file.eof()) {
 
+} 
+		while (tempStr < 65 && (!file.eof())) {
+			file.get(tempStr);
+		}
+
+		while (tempStr >= 65 && (!file.eof())) {
+			if (tempStr >= 65 && tempStr <= 90) {
+				word += tempStr;
+			}
+			if (tempStr >= 97 && tempStr <= 122) {
+				word += tempStr;
+			}
+
+			file.get(tempStr);
+		}
+
+		for (int i = 0; i < word.length(); i++) {
+			if (word[i] >= 65 && word[i] <= 90)
+				word[i] += 32;
+		}
+
+		(*tree).Add(word);
+		word = "";
+	}
+
+	return *tree;
 }
