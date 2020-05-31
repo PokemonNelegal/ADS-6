d6e04433a65c9170e1ffaa594160eab5abeee29b
ElizavetaR16 <62614466+ElizavetaR16@users.noreply.github.com>
2020-05-31 22:18:31 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -3,8 +3,40 @@
 #include  <fstream>
 #include  <locale>
 #include  <cstdlib>
+using namespace std;
+#include <cctype> 
 
 BST<std::string> makeTree(char* filename)
 {
-
+	BST<std::string>BS;
+	ifstream f;
+	f.open(filename);
+	string s = "", temp = "";
+	int i = 0;
+	bool flag = false;
+	while (f >> s)
+	{
+		for (int j = 0; j < s.size(); j++)
+		{
+			flag = false;
+			if ((s[j] >= 'a' && s[j] <= 'z') || (s[j] >= 'A' && s[j] <= 'Z'))
+				temp += (char)tolower(s[j]);
+			else
+			{
+				if (temp != "")
+				{
+					BS.add(temp);
+					temp = "";
+					flag = true;
+					continue;
+				}
+			}
+		}
+		if (flag == false && temp != "")
+		{
+			BS.add(temp);
+			temp = "";
+		}
+	}
+	return BS;
 }
