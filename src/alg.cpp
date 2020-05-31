15371d7b5329edaa910cc5511a222038062af973
dariavarentsova <63418397+dariavarentsova@users.noreply.github.com>
2020-05-31 23:08:20 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -6,5 +6,39 @@
 
 BST<std::string> makeTree(char* filename)
 {
+std::string word;
+	std::ifstream F(filename);
+	BST<std::string> *tree = new BST<std::string>;
+	char str = ' ';
+	while (!F.eof()) {
 
+		while (str < 'A' && (!file.eof()))
+		{
+			file.get(str);
+		}
+
+		while (str >= 'A' && (!file.eof()))
+		{
+			if (str >= 'A' && str <= 'Z')
+			{
+				word += str;
+			}
+			if (str >= 'a' && str <= 'z')
+			{
+				word += str;
+			}
+
+			file.get(str);
+		}
+		for (int i = 0; i < word.length(); i++)
+		{
+			if (word[i] >= 'A' && word[i] <= 'Z')
+				word[i] += 32;
+		}
+
+		(*tree).add(word);
+		word = "";
+	}
+
+	return *tree;
 }
