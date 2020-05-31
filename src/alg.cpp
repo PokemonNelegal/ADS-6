6e3d735ed21433ff725e1b3b658f35392ece9749
Nastua1309 <62643077+Nastua1309@users.noreply.github.com>
2020-05-31 13:22:44 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -8,3 +8,30 @@ BST<std::string> makeTree(char* filename)
 {
 
 }
+BST<std::string> tree;
+	ifstream file(filename);
+	string str,node;
+	while (!file.eof()) {
+		node="";
+		file >> str;
+		bool flag = 0;
+		for (int i = 0; i < str.length(); i++) {
+			if (isalpha(str[i]))
+			{
+				flag = 1;
+				str[i]=tolower(str[i]);
+				node += str[i];
+			}
+			else if (flag == 1)
+			{
+				tree.add(node);
+				node = "";
+				flag = 0;
+			}
+		}
+		if (!node.empty()) {
+			tree.add(node);
+		}
+	}
+	return tree;
+}
